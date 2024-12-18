7d620c201ae6dd18791936ba707dfedf7a4e3b3d
Nguyen Ba Bao Nam <namqx1234@gmail.com>
2024-12-18 15:06:37 +0700

    Update GG and Paypal

--- src/main/java/com/example/pizza/config/WebConfig.java
+++ src/main/java/com/example/pizza/config/WebConfig.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -33,4 +35,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
+    
 }
